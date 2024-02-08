618231f4b620a9280013eba2566a4cbe508f9955
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-02-08 18:01:03 +0900

    .

--- dream2/js/main.js
+++ dream2/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     
     function scroll_chk(){ // 함수의 선언
         scrolling = $(window).scrollTop()
-        console.log(scrolling)
+        // console.log(scrolling)
         if(scrolling > 0){ // 스크롤 된 값이 0보다 크다면 - fixed 클래스 추가
             $('.header').addClass('fixed')
         }else{ // 0이거나 0보다 작거나 - fixed 클래스 삭제
@@ -21,8 +21,28 @@ $(document).ready(function(){
     }) // $(window).scroll
 
     /* pc버전시 메뉴에 오버하면 header 스타일 변경 */
+    let window_w // 브라우저의 넓이
+    let device_status // 모바일모드인지 pc모드인지 구분
+    let mobile_size = 640
+    function device_chk(){
+        window_w = $(window).width() // 브라우저 넓이 저장
+        if(window_w > mobile_size){ // pc버전
+            device_status = 'pc'
+        }else{ // 640부터 그 이하
+            device_status = 'mobile'
+        }
+        console.log(device_status)
+    } //function device_chk
+    device_chk() // 함수 호출
+    $(window).resize(function(){
+        device_chk()
+    })
+
     $('.header .gnb').on('mouseenter focusin', function(){
-        $('.header').addClass('menu_over')
+        $('.header').addClass('menu_over') // pc일때만 실행
+        if(device_status == 'pc'){
+            $('.header').addClass('menu_open')
+        }
     })
     $('.header').on('mouseleave', function(){
         $('.header').removeClass('menu_over')
@@ -33,4 +53,44 @@ $(document).ready(function(){
         $('.header').removeClass('menu_over')
     })
 
+    /* 모바일 메뉴 열기 / 닫기 */
+    $('.header .gnb .gnb_open').on('click', function(){
+        $('.header').addClass('menu_open')
+    })
+    $('.header .gnb .gnb_close').on('click', function(){
+        $('.header').removeClass('menu_open')
+    })
+
+    /* <li>                           -- sub_open 클래스 추가
+           <a href=#n>1차메뉴</a>      -- .header .gnb ul.depth1 > li > a
+           <ul class="depth2">
+           </ul>
+       </li>
+       1차메뉴 a를 클릭하면 감싸는 li에 클래스 추가 
+       a는 href 이동값을 가지고 있음 이게 모바일에서 클릭되면 안됨 */
+       
+    $(".header .gnb ul.depth1 > li > a").on("click", function(e){
+        if(device_status == 'mobile'){
+            e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+            $(this).parent().toggleClass('sub_open')
+        }
+	});
+
+    /* top버튼을 클릭하면 상단으로 자동 스크롤 */
+    $('.footer .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+
+    /* 팝업 */
+    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });
+    
 }) // $(document).ready
