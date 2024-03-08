8cbf9f5a9548e576933293f8f5bb5b290f95d891
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-03-09 01:25:45 +0900

    .

--- 제출용/js/main.js
+++ 제출용/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('.header .gnb').on('mouseleave', function(){
         $('.header .gnb .depth1 > li').removeClass('on')
     })
-    $('.header .gnb .depth1 > li:last-child > ul > li:last-child > a').on('mouseenter focusin', function(){
+    $('.header .gnb .depth1 > li:last-child > ul > li:last-child > a').on('focusout', function(){
         $('.header .gnb .depth1 > li').removeClass('on')
         $(this).addClass('on')
     })
