7f954114e2752808c1f9a8a37e34dbba1d635cc7
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-03-09 22:18:56 +0900

    .

--- 제출용/js/main.js
+++ 제출용/js/main.js
@@ -10,34 +10,45 @@ $(document).ready(function(){
         $('.header .gnb .depth1 > li').removeClass('on')
         $(this).addClass('on')
     })
-    $('.containner .board .list > li').on('click', function(){
+    $('.container .board .list > li').on('click', function(){
         $('.container .board .list > li').removeClass('on')
         $(this).addClass('on')
     })
-    $('.notice .leyar_open').on('click', function(){
+    $('.notice .layer_open').on('click', function(){
         $('.layer_popup').show()
     })
     $('.layer_popup .close button').on('click', function(){
         $('.layer_popup').hide()
     })
 
-    let idx = 1
-    let obj_left = 0
-    setInterval(function(){
-        if(idx > 3){
-            idx++
-        }else{
-            idx = 1
-        }
-        /* 1번 팝업 = left 0 
-           2번 팝업 = left -1200
-           3번 팝업 = left -2400 */
-        obj_left = -(inx-1)*1200
-            
-        $('visual ul').animate({
-            left : obj_left
-        }, 500)
-    }, 1000)
+    /* console 전부 지우고 저장해야함
+       페이드 인아웃 효과  */
+       let idx = 1
+       setInterval(function(){
+           /* 팝업순 1-2-3-1-2-3 */
+           if(idx < 3){
+               idx++
+           }else{
+               idx = 1
+           }
+           //console.log(idx)
+           $('.visual ul li').removeClass('active')
+           $('.visual ul li').eq(idx-1).addClass('active')
+       }, 3000)
+   })
 
-
-})
+/* console 전부 지우고 저장해야함
+   슬라이드 효과  */
+//    let idx = 1
+//    let obj_left = 0
+//    setInterval(function(){
+//        if(idx < 3){
+//            idx++
+//        }else{
+//            idx = 1
+//        }
+//        /* 1번 팝업 */
+//        $('.visual ul li').removeClass('active')
+//        $('.visual ul li').eq(idx-1).addClass('active')
+//    }, 3000)
+// })
