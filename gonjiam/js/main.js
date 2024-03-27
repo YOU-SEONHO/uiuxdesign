96a1d47fa5e82c8df3b9654d7c5cec4ceb7a6783
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-03-27 18:11:21 +0900

    .

--- gonjiam/js/main.js
+++ gonjiam/js/main.js
@@ -3,3 +3,60 @@
    작성일 : 2024.03.20
    속  성 : 메인페이지에서 사용된 jqeury (header 빼고) */
 
+$(document).ready(function(){
+   const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+      autoplay: {  /* 팝업 자동 실행 */
+         delay: 3000,
+         disableOnInteraction: true,
+      },
+
+      loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+      pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+         el: '.swiper-pagination', /* 해당 요소의 class명 */
+         clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+      },
+   });
+
+   const biz_swiper = new Swiper('.biz .swiper', { /* 팝업을 감싼는 요소의 class명 */
+      slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+      spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+      breakpoints: {
+         768: {    /* 768px 이상일때 적용 */
+            slidesPerView: 3,
+            spaceBetween: 28,
+         },
+         1024: {    /* 768px 이상일때 적용 */
+            slidesPerView: 4,
+            spaceBetween: 25,
+         },
+         1320: {    /* 768px 이상일때 적용 */
+            slidesPerView: 4,
+            spaceBetween: 28,
+         },
+      },
+      navigation: {
+         nextEl: '.biz .next',
+         prevEl: '.biz .prev',
+      },
+   });
+
+   const room_swiper = new Swiper('.room .swiper', { /* 팝업을 감싼는 요소의 class명 */
+      slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+      spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+      breakpoints: {
+         1320: {    /* 1280px 이상일때 적용 */
+            slidesPerView: 3,
+            spaceBetween: 108,
+         },
+      },
+      centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+      loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+      autoplay: {  /* 팝업 자동 실행 */
+         delay: 3000,
+         disableOnInteraction: true,
+      },
+   });
+
+})
