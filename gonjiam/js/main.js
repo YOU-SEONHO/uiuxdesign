19fc8426aedf8d62c48e32132cd291686690febc
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-03-28 18:07:10 +0900

    .

--- gonjiam/js/main.js
+++ gonjiam/js/main.js
@@ -43,20 +43,28 @@ $(document).ready(function(){
    });
 
    const room_swiper = new Swiper('.room .swiper', { /* 팝업을 감싼는 요소의 class명 */
-      slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+      centeredSlides: true,
+      slidesPerView: 'auto',
       spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
       breakpoints: {
-         1320: {    /* 1280px 이상일때 적용 */
-            slidesPerView: 3,
+         360: {    /* 360px 이상일때 적용 */
+            spaceBetween: 108,
+         },
+         768: {    /* 768px 이상일때 적용 */
+            spaceBetween: 108,
+         },
+         1024: {    /* 1024px 이상일때 적용 */
+            spaceBetween: 108,
+         },
+         1320: {    /* 1320px 이상일때 적용 */
             spaceBetween: 108,
          },
       },
-      centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-      loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-      autoplay: {  /* 팝업 자동 실행 */
-         delay: 3000,
-         disableOnInteraction: true,
-      },
+      loop: true,  
+      autoplay: {
+		delay: 3000,
+		disableOnInteraction: true,
+	   },
    });
 
 })
