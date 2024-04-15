d985321a8f671539ffa38ad400379a8fd9061100
YOU-SEONHO <157003568+YOU-SEONHO@users.noreply.github.com>
2024-04-16 00:31:49 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -4,17 +4,17 @@
    속  성 : 메인페이지에서 사용된 jqeury (header 빼고) */
 
 $(document).ready(function(){
-   const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
+   const myFullpage = new fullpage('#fullpage', {
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
-		navigationPosition: 'right', /* 위치 */
-		navigationTooltips: ['HOME', 'ABOUT', 'PORTFOLIO1', 'PORTFOLIO2', 'CONTACT'], /* 툴팁 */
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['HOME', 'ABOUT', 'PORTFOLIO1', 'PORTFOLIO2', 'CONTACT'],
+		showActiveTooltip: true,
 
-		autoScrolling:true, /* 한페이지씩 스크롤 */
+		autoScrolling:true,
 		scrollHorizontally: true,
 
-		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
+		verticalCentered: true,
 	
    });
 })
